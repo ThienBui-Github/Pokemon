4b3028cb23a3ff1e084bf34e51637c7c69c34e0c
Thien Bui <THB_13@hotmail.com>
2022-01-30 20:58:39 -0500

    Uncomment NavLink for Galar region

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -31,9 +31,9 @@ const Nav = () => {
         <Menu.Item as={NavLink} to="/region/Alola">
           Alola
         </Menu.Item>
-        {/* <Menu.Item as={NavLink} to="/region/Galar">
+        <Menu.Item as={NavLink} to="/region/Galar">
           Galar
-        </Menu.Item> */}
+        </Menu.Item>
       </Menu>
     </nav>
   );
